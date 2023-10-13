34206478a1b452f85c6ccf66b99aaa5f88dc9ae5
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-10-13 11:48:44 -0400

    Update auto-aws_cli.sh

--- auto-aws_cli.sh
+++ auto-aws_cli.sh
@@ -3,6 +3,18 @@
 sudo apt update
 sudo apt install unzip
 
+# Download AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+sudo apt update
+
+# Remove files if they exist
+rm -f ~/.aws/credentials 
+rm -f ~/.aws/config
+
+# Download config file from s3 bucket & replace 
+curl -O https://dp5-auto1.s3.amazonaws.com/config.txt
+cp config.txt ~/.aws/config
+
+sudo apt update
