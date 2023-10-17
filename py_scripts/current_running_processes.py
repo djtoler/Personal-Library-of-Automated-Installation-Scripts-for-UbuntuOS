00a762c23ad18721ff443adc3fe298aa7d82510f
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-10-17 16:10:56 -0400

    Update current_running_processes.py

--- py_scripts/current_running_processes.py
+++ py_scripts/current_running_processes.py
@@ -1,55 +1,53 @@
-# Run pip install psutil as root user & run file with sudo or else you'll get permission denied 
+# Run pip install psutil and change file permission to execute file if needed 
 
+#import the following python modules
 from datetime import datetime
 import psutil
 import csv
 
-# Store the name of the file in a variable
+#Set variable to date and time to be used later in the file name
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 filename = f"current_running_process_{current_datetime}.csv"
 
-# Store the list of process_ids in a variable
+#Set the list of process_ids to a variable
 processes = psutil.pids()
 
-
+#Define a fuction:
 def current_running_processes():
-
+    #Incorporate error handling
     try:
+      #Open a CSV file to write into
         with open (filename, 'w', newline='') as file:
-                # Pass our running_processes.csv file to our csv writer object
+                #Create a writer object variable to write into the csv
                 writer = csv.writer(file)
 
-                # Store the column headers in a variable
+                #Set the column headers to a variable
                 field = [ "process_id", "process_name", "process_path", "process_cpu", "process_memory"]
                 
-                # Write the column names horizontally as headers
+                # Write into the csv row the column headers
                 writer.writerow(field)
 
-                #Loop over the list of process ids
+                #Loop through each process
                 for process in processes:
 
-                    #Store the collection of data for each process in a variable
+                    #Set a variable to that allows the access of information within the psutil library and Process class
                     process_id_data = psutil.Process(process)
 
-                    #Store the id, name, path, cpu, memory of each process id in seperate variables
-                    process_id= process
-                    process_name= process_id_data.name()
-                    process_path= process_id_data.exe()
-                    process_cpu= process_id_data.cpu_percent()
-                    process_mem= process_id_data.memory_percent()
+                    #Set id, name, path, cpu, memory to a variables
+                    process_id = process
+                    process_name = process_id_data.name()
+                    process_path = process_id_data.exe()
+                    process_cpu = process_id_data.cpu_percent()
+                    process_mem = process_id_data.memory_percent()
 
-                    #Put each process data point in a list and pass it to the writerow method to be wriote into the csv file
+                    #Write into the csv row the following
                     writer.writerow([process_id, process_name, process_path, process_cpu, process_mem])
-
-    except(psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+    #For the following exceptions:
+    except(psutil.NoSuchProcess, psutil.AccessDenied):
+        #return none of each field
          return None, None, None, None, None
          
-
-    
-
 current_running_processes ()
-
-
 
 
 
