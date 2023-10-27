b1797936e0f1d55b1ff57efb41d8650e519d9c5f
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-10-26 20:37:52 -0400

    Update auto-python.sh

--- auto-python.sh
+++ auto-python.sh
@@ -8,6 +8,8 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update -y
 sudo apt install -y python3.7 python3.7-venv
+sudo apt install -y python3.7-dev
+
 
 # Install Python3 pip
 sudo apt install python3-pip -y
