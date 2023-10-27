98ff9000e2aafb0efa6f9f8049419eef0b1f0524
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-10-27 00:09:18 -0400

    Update banking-app.sh

--- apps/banking-app.sh
+++ apps/banking-app.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-touch test1.txt
+touch tebkbjhbst1.txt
 
-touch test.txt
+touch tehbhst.txt
+
 python3.7 -m venv test
 pip install gunicorn
 pip install mysqlclient
 source test/bin/activate
 git clone https://github.com/djtoler/Deployment6.git
 cd Deployment6
-pip install pip --upgrade
 pip install -r requirements.txt
 python database.py
 sleep 1
 python load_data.py
 sleep 1 
-python -m gunicorn app:app -b 0.0.0.0 -D && echo "Done"
+python -m gunicorn app:app -b 0.0.0.0 -D
+echo "Done"
