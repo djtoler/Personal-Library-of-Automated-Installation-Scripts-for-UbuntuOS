91d8b4b71315fad92470315c8e286abc3de4a1c0
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-11 00:35:32 -0500

    Create kill-react_app.sh

---
+++ py_scripts/kill-react_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Find the process ID of the frontend app
+PID=$(lsof -i :3000 -t)
+
+# If its found, write it to a file, and kill it
+if [[ -n $PID ]]
+then
+    echo $PID > pid.txt
+    kill -9 $PID
+fi
