c15208fb78706afa8107948e4e44c52f62ccf7ca
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-18 15:01:54 -0500

    Update auto-nodejs_install.sh

--- auto-nodejs_install.sh
+++ auto-nodejs_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install npm -y
+#Install NodeJS specific version
+#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+#sudo apt-get install npm -y
+
+#Install Node Version Manager(NVM)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 10
+nvm use 10
