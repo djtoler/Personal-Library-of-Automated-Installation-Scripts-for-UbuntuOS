1a3064d8dcbd004f516103a3d4a359f11b53bb2d
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-11-30 02:49:47 -0500

    Update auto-github_repo_create.sh

--- auto-github_repo_create.sh
+++ auto-github_repo_create.sh
@@ -6,3 +6,9 @@ REPO_NAME="$2"
 curl -H "Authorization: token $GH_TOKEN" \
      -d "{\"name\": \"$REPO_NAME\",\"private\":false,\"auto_init\":true,\"}" \
      https://api.github.com/user/repos
+
+if git remote | grep -q 'origin'; then
+    git remote remove origin
+fi
+
+git remote add origin https://github.com/djtoler/$REPO_NAME
