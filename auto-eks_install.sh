a49ee2ecac45c9e4021fc7752f300bfa3a4bac55
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-12-08 03:03:53 -0500

    Create auto-eks_install.sh

---
+++ auto-eks_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Install and configure Kubernets/EKS
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
