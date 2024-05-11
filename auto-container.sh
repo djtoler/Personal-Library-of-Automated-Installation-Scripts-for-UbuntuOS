b21aabf7f5b8ed98c90d4e63cd4ad3ef4b204859
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2024-05-11 17:24:48 -0400

    Update auto-container.sh

--- auto-container.sh
+++ auto-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker login
-curl -O https://raw.githubusercontent.com/djtoler/hwthdemo3/main/Dockerfile
-docker build -t djtoler/dk8001 
-docker push djtoler/dk8001:latest
-docker run -d -p 80:80 djtoler/dk8001:latest
+curl -O https://raw.githubusercontent.com/djtoler/hwthdemo4/main/Dockerfile
+docker build -t djtoler/dk9999 . 
+
+docker run -d -p 80:80 djtoler/dk9999:latest
